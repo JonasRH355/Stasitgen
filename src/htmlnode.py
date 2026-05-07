57889232c1a7b8765f75b2fa0523f75790f2cef9
JonasRH355 <jonatas.hellmann355@gmail.com>
2026-05-07 11:19:04 -0300

    It was made the properly class to the leaf nodes, and more tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,5 +22,17 @@ class HTMLNode():
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
-    
- 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None or self.tag == "":
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__():
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
