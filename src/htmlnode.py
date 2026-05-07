d810658dadb0623f07bb16a7247c480b0369a36a
JonasRH355 <jonatas.hellmann355@gmail.com>
2026-05-07 10:31:20 -0300

    htmlNode created, its not done yet, but I've already started

---
+++ src/htmlnode.py
@@ -0,0 +1,26 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+
+        formatted = ""
+
+        for attribute in self.props:
+            formatted += f" {attribute}=\"{self.props[attribute]}\""
+
+        return formatted 
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+    
+ 
