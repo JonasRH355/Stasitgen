885c2939e2d97b2f6f5bde5a500eb6ef7a61c59a
JonasRH355 <jonatas.hellmann355@gmail.com>
2026-05-18 18:01:44 -0300

    Extraction of imgs and links using regex

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,5 @@
 import unittest
-from inline_markdown import (
-    split_nodes_delimiter,
-)
+from inline_markdown import *
 
 from textnode import TextNode, TextType
 
@@ -85,6 +83,38 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(text)
+        self.assertAlmostEqual(
+            result, 
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif")
+                , ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ]
+        )
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://wikipedia.org"),
+            ],
+            matches,
+        )
+
+
+
 
 
 if __name__ == "__main__":
