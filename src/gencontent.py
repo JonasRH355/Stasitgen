4b658a4076f5f3a581bba600cd5870f608ae6be0
JonasRH355 <jonatas.hellmann355@gmail.com>
2026-05-25 15:04:35 -0300

    I tired to fix some bugs about links, css and ims

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 from block_markdown import markdown_to_html_node
 
-def generate_page(frm,template, dest):
-    print(f"Generating page from {frm} to {dest} using {template}")
+def generate_page(frm,template, dest, basepath):
+    print(f"Generating page from {frm} to {dest} using {template}")  
     from_file = open(frm, "r")
     markdown_content = from_file.read()
     from_file.close()
@@ -18,22 +18,32 @@ def generate_page(frm,template, dest):
     
     template_content = template_content.replace("{{ Title }}", title )
     template_content = template_content.replace("{{ Content }}", html_string)
-    
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
+
     dest_dir_path = os.path.dirname(dest)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest, "w")
     to_file.write(template_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(
+                from_path, 
+                template_path, 
+                dest_path, 
+                basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(
+                from_path, 
+                template_path, 
+                dest_path, 
+                basepath)
        
 
 def extract_title(md):
--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
 from textnode import TextNode, TextType
 from os import path, listdir, mkdir
 from shutil import copy, rmtree 
+import sys
 
 from copystatic import *
 from gencontent import *
 
 dir_static = "./static"
-dir_public = "./public"
+dir_public = "./docs"
 dir_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     if path.exists(dir_public):
-        rmtree("./public")
+        rmtree(dir_public)
     copy_files_recursive(dir_static, dir_public)
 
     generate_pages_recursive(
         dir_content,
         template_path,
-        dir_public
+        dir_public,
+        basepath
     )
 
 
